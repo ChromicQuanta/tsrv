d2a7ef023cca00ad4b844b7db23073c737c880e0
ChromicQuanta <59491666+ChromicQuanta@users.noreply.github.com>
2026-03-04 23:49:20 +1100

    Update forward.js

--- forward.js
+++ forward.js
@@ -1,9 +1,35 @@
 console.log("ee")
 ws = require("ws")
 console.log("ff")
+war = []
 S = new ws.Server({port:80})
-sws = {send:()=>{}}
+pws = {send:()=>{}}
 S.on("connection",(ws)=>{
-  ws.onmessage
+  war.id=war.push(ws)-1
+  ws.hasPaired = 0
+  ws.onmessage=(d)=>{
+    if(d.data=="prime"){
+      pws = d.target
+    }
+    if(d.data=="client"){
+      sws.send("Client "+d.target.id)
+    }
+
+      if(d.target.hasPaired){
+war[d.target.pair].send(d.data)
+      }
+    if(d.data.startsWith("server")){
+      id = +d.data.split(" ")[1]
+      war[id].pair = d.target.id
+      d.target.pair = id
+      war[id].hasPaired = 1
+      d.target.hasPaired = 2
+    }
+    
+
+    
+  }
+  
+  
   ws.send()
 })
